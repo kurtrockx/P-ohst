61ae47b600f84cb3e42038a97cf645017c0bcc26
derpfear <kurtdebelen431@gmail.com>
2025-04-18 11:49:48 +0800

    added first draft of post form submission layout

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,9 +1,20 @@
 function NewPost() {
   return (
-    <div>
-      
-    </div>
-  )
+    <form className="mx-auto flex w-max flex-col p-4">
+      <label>Image Attachment</label>
+      <input
+        type="file"
+        className="cursor-pointer rounded-md border bg-white px-2 py-1"
+      />
+      <label>Post Title</label>
+      <input
+        type="text"
+        className="rounded-md border bg-white px-2 outline-0"
+      />
+      <label>Description</label>
+      <textarea className="min-h-48 resize-none rounded-md border bg-white p-px outline-0"></textarea>
+    </form>
+  );
 }
 
-export default NewPost
+export default NewPost;
